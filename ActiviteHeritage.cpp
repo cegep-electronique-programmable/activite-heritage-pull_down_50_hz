11b63410028fcd808aa81e982cd9fe23f4164996
Etienne-Vallee-04 <123583583+Etienne-Vallee-04@users.noreply.github.com>
2024-05-08 14:03:37 -0400

    créer la classe

--- ActiviteHeritage.cpp
+++ ActiviteHeritage.cpp
@@ -6,14 +6,13 @@ class etudiant {
     int moyenne;
 	
 public:
-    etudiant(double);
     double getMatricule();
     void setMoyenne(int);
     int getMoyenne();
+    void etudiantsetMatricule(double);
 };
 
-
-etudiant::etudiant(double nouveauMatricule){
+void etudiant::etudiantsetMatricule(double nouveauMatricule){
     //Le numÃ©rod de matricule doit nÃ©cessairement avoir 6 chiffres
     if ((nouveauMatricule >= 100000) && (nouveauMatricule <= 999999)){
         this->numeroMatricule = nouveauMatricule;    
@@ -21,13 +20,11 @@ etudiant::etudiant(double nouveauMatricule){
     else{
         this->numeroMatricule = 0;
     }
-    
 }
 
 double etudiant::getMatricule(void){
     return (this->moyenne);
 }
-
 
 void etudiant::setMoyenne(int nouvelleMoyenne){
     if (nouvelleMoyenne <= 100){
@@ -38,17 +35,43 @@ void etudiant::setMoyenne(int nouvelleMoyenne){
     }
 }
 
-
 int etudiant::getMoyenne(void){
     return (this->numeroMatricule);
 }
+//classe de mon cours de sabre laser
+class Cours_SabreLaser:public etudiant
+{
+    private:
+        int note_etudiant;
+        int nbr_victoire;
+        int nbr_defaite;
+        int nbr_egalite;
+        int nbr_combat;
+        const char* competance_aquise;
+        void classement_eleve(int,int,int,int);
+    
+    public:
+        const char* type_sabrelaser;
+        const char* couleur_sabrelaser;
+        const char* couleur_lame;
+        const char* style_combat;
+        void set_nbr_combat(int nbr_combat){
+        this -> nbr_combat = nbr_combat;
+        }
+        int get_nbr_combat(){return nbr_combat;}
+};
     
 
 int main() {
     printf("Hello world !\n\r\n\r");
-    etudiant etudiant147(147);
+    Cours_SabreLaser etudiant147;
+    etudiant147.etudiantsetMatricule(147);
     etudiant147.setMoyenne(100);
     printf("Etudiant147, matricule # %d, moyenne %d", etudiant147.getMatricule(), etudiant147.getMoyenne());
+    
+    etudiant147.set_nbr_combat(10);
+    printf ("\rEtudiant147 a fait %d combat", etudiant147.get_nbr_combat());
+    
     
     return 0;
 } 

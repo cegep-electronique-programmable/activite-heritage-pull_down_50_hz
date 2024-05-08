c006ad2322cc3cad92ca64b3a92cd28ad5edda08
Gillaume-Manu <159054896+Gillaume-Manu@users.noreply.github.com>
2024-05-08 13:11:26 -0400

    Update ActiviteHeritage.cpp

--- ActiviteHeritage.cpp
+++ ActiviteHeritage.cpp
@@ -23,50 +23,7 @@ public:
     }
 }; 
 
-// avion hÃ©rite publiquement de vÃ©hicule.
-class avion:public vehicule {
-    int nbre_moteurs;
-
-public:
-    void init_avion(int nm) 
-    {
-        nbre_moteurs = nm;
-    }
-};
-
-// moto hÃ©rite publiquement de vÃ©hicule.
-class moto:public vehicule 
-{
-    double cylindree;
-
-public:
-    void init_moto(double cy) 
-    {
-        cylindree = cy;
-    }
-};
-
 int main() {
-    avion boeing767;
-    boeing767.init_avion(2);
-    // Fonctions hÃ©ritÃ©es de la classe de base.
-    boeing767.init_vehicule(950,200);
-    boeing767.affiche(); // affiche: 950, 200 mais pas 2.
+    
     return 0;
 } 
-
-/*
-int main() {
-    avion boeing767;
-    boeing767.init_avion(2);
-    // Fonctions hÃ©ritÃ©es de la classe de base.
-    boeing767.init_vehicule(950,200);
-    boeing767.affiche(); // affiche: 950, 200 mais pas 2.
-    // Err: accÃ¨s Ã  des donnÃ©es privÃ©es de la classe de base.
-    boeing767.nbre_moteurs=4;
-    boeing767.vitesse=800;
-    // Err: accÃ¨s Ã  des donnÃ©es privÃ©es de la classe dÃ©rivÃ©e.
-    boeing767.nbre_passagers=188;
-    return 0;
-} 
-*/
